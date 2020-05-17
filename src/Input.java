34daf10ee14b66d64010aa16fbfe64d313e560ce
Puhiza <57593327+Puhiza@users.noreply.github.com>
2020-05-17 17:19:44 +0200

    Add files via upload

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,30 @@
+import javax.swing.*;
+public class Input {
+   static double loan;
+   static double annual_rate;
+   static double monthly_rate;
+   static int months;
 
+
+   public static void insert() {
+      try {
+      
+         loan = Double.parseDouble(JOptionPane.showInputDialog("Write the loan amount in euros: "));
+         annual_rate = Double.parseDouble(JOptionPane.showInputDialog("Write the annual interest rate: "));
+         months = Integer.parseInt(JOptionPane.showInputDialog("Write the number of months: "));
+         monthly_rate=annual_rate/1200;
+      
+        
+         if(loan<0 || annual_rate<0||months<0) {
+            JOptionPane.showMessageDialog(null, "The following values should not be negative!");
+            insert();}
+      
+      	
+      }
+      catch(Exception e) {
+         JOptionPane.showMessageDialog(null, "Please write a valid numerical value!");
+         insert();}
+   
+   	
+   }  
+}
